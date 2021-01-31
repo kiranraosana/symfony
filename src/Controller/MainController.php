34b5007926d15321fcad466022f92b311fd58f75
Kiran <kiranrao.sana@gmail.com>
2021-01-31 18:03:09 +0530

    symfony

---
+++ src/Controller/MainController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('main/index.html.twig');
+    }
+
+     /**
+     * @Route("/custom/{name?}", name="custom")
+     * @param Request $request
+     * @return Response
+     */
+    public function custom(Request $request)
+    {
+        dump($request);
+    	$name = $request->get('name');
+    	return new Response('<h2>Hi' . $name . ' :)</h2>');
+    }
+
+}
